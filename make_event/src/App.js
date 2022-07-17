5b2e91cfa6d2d370d8f05bb30ad8d9bbdecf1ff1
asdf8397 <scv2314@naver.com>
2022-07-17 16:51:20 +0900

    yarn_RERE

--- make_event/src/App.js
+++ make_event/src/App.js
@@ -1,30 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-function Header() {
+function Header(props_title) {
   return(
     <header>
-      <h1><a href="/">WEB</a></h1>
+      <h1><a href="/">{props_title.title}</a></h1>
     </header>
   )
 }
-function Nav() {
+function Nav(props_NavList) {
+  const NavList = []
+  for(i = 0; i < props_NavList.arrayList.length; i++) {
+    let listSum = props_NavList.arrayList[i]; 
+    // 가져와서 lstSum에 넣음 뿌려줄때 listSum을 활용하면 된다.
+    NavList.push(<li key={listSum.id}><a href={"/read/" + listSum.id}>{listSum.title}</a></li>)
+  }
   return (
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
+      {NavList}
     </ol>
   );
 }
-function Article() {
+function Article(props_article) {
   return (
     <article>
-      <h2>Welcome</h2>
-      Hello, WEB
+      <h2>{props_article.title}</h2>
+      {props_article.body}
     </article>
   )
 }
 function App() {
+  const arrayList = [
+    {id:1, title:"html", body:"html is ...."},
+    {id:2, title:"css", body:"css is ...."},
+    {id:3, title:"js", body:"js is ...."}
+  ]
   return (
     /** mode 변경 기능 (컴포넌트 추가, onChangeMode 이벤트 추가 (핸들러))
      * 
@@ -37,9 +46,9 @@ function App() {
      */
     /* 원래 onChange인데 onchangeMode는 화살표 함수의 받을것을 설정함 */
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="안녕하십니까 웹입니다. a태그 추가하면 클릭되지롱"></Header>
+      <Nav topics ={arrayList}></Nav>
+      <Article title="안녕하세요요요요" body="웹 이지롱"></Article>
     </div>
   );
 }
--- make_props/src/type_change_props.js
+++ make_props/src/type_change_props.js
@@ -9,7 +9,25 @@ function Header(props_title) {
 }
 function Nav(props_Header) {
     const list_Arr = []
-    for(let i = 0; i<)
+    for(let i = 0; i < props_Header.topics.length; i++) { 
+        // 여기서 반복문의 topics는 <Nav topics></Nav>로 설정해준 변수이다.
+        /* for 반복문에서 props_Header.topics.length를 설정하는 것은 
+        Nav의 설정한 li를 Nav(props_Header)에 담았는데 이 값을 빼주는데 
+        변수를 지정한 topics배열만 빼줘야하기 떄문에 .topics라고 해줌 */
+        let Nav_input = props_Header.topics[i]; // 반복문을 돌려준 리스트 - 이것을 주로 활용함
+        /* let Nav_input을 변수선언하고 props_Header.topics[i]를
+            넣은 이유는 function Nav()의 안에 들어가있는 li의 리스트를
+            가져와서 topics를 i에 맞게 반복문을 진행줘야 하기 때문이다.
+             */
+        list_Arr.push(<li key={Nav_input.id}>{Nav_input.title}</li>) // 1번
+        list_Arr.push(<li key={Nav_input.id}><a href={"/read/" + Nav_input.id}>{Nav_input.title}</a></li>) // 2번
+
+        /* list_Arr.push의 1번과 2번의 차이는 */
+        // 1번은 그냥 출력 ---- id와 title만 간단히 추가
+        // 2번은 클릭을 할 수 있게 출력 링크를 추가한 출력이다. --- id와 title a태그를 이용한 read기능을 추가함
+        
+        // 선언후 list_Arr에 넣음
+    }
     return (
         /** return안에서 이렇게 묶어서 설정할 수 도 있지만 
          * 다르게 처리 가능하다.
@@ -22,18 +40,26 @@ function Nav(props_Header) {
         </nav>
         */
 
+        <ol>
+            {list_Arr} 
+            {/* 이부분이 return이 되서 Nav()에 담기고 App()부분에서 뿌려지게 된다. */}
+        </ol>
     );
 }
-function Article() {
+function Article(props) {
     return (
         <article>
-            <h2>안녕하세요요</h2>
-            안녕 웹입니다.
+            <h2>{props.title}</h2>
+            {props.body}
         </article>
     );
 }
 function OutTypeApp() {
-    const topics = [
+    const topics = [ 
+        /* li를 const topics[] 배열에 넣고 for 반복문을 통해서 출력후
+            지정한 변수의 배열에 넣어서 밖으로 빼주면서
+            OutTypeApp()의 함수의 Nav에 지정해 줄 수 있음
+        */
         {id:1, title:"html", body:"html is ..."},
         {id:2, title:"css", body:"css is ..."},
         {id:3, title:"js", body:"js is ..."}
@@ -41,8 +67,9 @@ function OutTypeApp() {
     return(
         <div>
             <Header title="안녕하십니까 웹입니다."></Header>
-            <Nav></Nav>
-            <Article></Article>
+            <Nav topics={topics}></Nav> 
+            {/* 여기 Nav */}
+            <Article title="안녕하세요요요요" body="안녕 웹입니다."></Article>
         </div>
     );
 }

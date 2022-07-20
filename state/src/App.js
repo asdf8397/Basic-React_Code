d6fbecd90e02b2189c2ce12cd485677112ea42ff
asdf8397 <scv2314@naver.com>
2022-07-20 13:43:04 +0900

    2022.07.20 github_upload

--- state/src/App.js
+++ state/src/App.js
@@ -7,7 +7,29 @@ import './App.css';
  * prop는 component를 사용하는 외부자를 위한 데이터
  * state는 component를 사용하는 내부자를 위한 데이터
  */
+function Article(props) {
+  return(
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
+function Header(props) {
+  return (
+    <header>
+      <h1><a href="/" onClick={(event) => {
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
+    </header>
+  )
+}
+function Nav(props) {
+
+}
 function App() {
+  const 
   return (
     <div>
       <Header></Header>
